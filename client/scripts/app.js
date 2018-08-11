7522948b4388748063125ea13730206e742c14d3
kaycheez <kevin.m.chau@gmail.com>
2018-08-11 16:58:49 -0700

    MVP accomplished

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,12 +4,23 @@ var app = {
   init: function () {
     $('#send').on('submit', app.handleSubmit);
     app.currentUserName = window.location.search.split('=')[1];
-    console.log(app.currentUserName);
+    app.fetch();
+    // console.log(app.currentUserName);
   },
 
   currentRoomName: 'Default Room',
 
   currentUserName: null,
+
+  currentData: null,
+
+  allRooms: {
+    'Default Room': 'Default Room',
+  },
+
+  friends: {
+
+  },
 
   send: function (message) {
     $.ajax({
@@ -20,6 +31,7 @@ var app = {
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
+        app.fetch();
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -29,22 +41,24 @@ var app = {
   },
 
   fetch: function () {
-    var fetchedData = $.get('http://parse.sfm8.hackreactor.com/chatterbox/classes/messages?order=-createdAt');
-    console.log(fetchedData);
-    // $.ajax({
-    //   // This is the url you should use to communicate with the parse API server.
-    //   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-    //   type: 'GET',
-    //   data: data,
-    //   contentType: 'application/json',
-    //   success: function (data) {
-    //     console.log('chatterbox: messages fetched');
-    //   },
-    //   error: function (data) {
-    //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    //     console.error('chatterbox: Failed to fetch messages', data);
-    //   }
-    // });
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages?order=-createdAt',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        app.currentData = data;
+        console.log(app.currentData);
+        app.handleRefresh(data);
+        //app.filterMessagesByRoom(data);
+        app.filterExistingRooms(data);
+        console.log(app.allRooms);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
   },
 
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
@@ -54,11 +68,53 @@ var app = {
   },
 
   renderMessage: function (message) {
-    text = message.text;
-    username = message.username;
-    roomname = message.roomname;
-    $message = $(`<div>${text}<div class="username" onclick="app.handleUsernameClick()">${username}</div>${roomname}</div>`);
+    text = message.text || '';
+    // if thew text includes <,>,/,\,",',& dont include it
+    for (var i = 0; i < text.length; i++) {
+      if (text[i] === '<' || text[i] === '>' || text[i] === '&' || text[i] === '"' || text[i] === '\'' || text[i] === '\\' || text[i] === '/') {
+        text = 'pwn denied';
+        break;
+      }
+    }
+    username = message.username || 'Anonymous';
+    for (var i = 0; i < username.length; i++) {
+      if (username[i] === '<' || username[i] === '>' || username[i] === '&' || username[i] === '"' || username[i] === '\'' || username[i] === '\\' || username[i] === '/') {
+        username = 'pwn denied';
+        break;
+      }
+    }
+    roomname = message.roomname || 'Default room';
+    for (var i = 0; i < roomname.length; i++) {
+      if (roomname[i] === '<' || roomname[i] === '>' || roomname[i] === '&' || roomname[i] === '"' || roomname[i] === '\'' || roomname[i] === '\\' || roomname[i] === '/') {
+        roomname = 'pwn denied';
+        break;
+      }
+    }
+    $username = $(`<div class="username" onclick="app.handleUsernameClick('${username}')">${username}</div>`);
+    if (app.friends[username]) {
+      $username.addClass('friend');
+    }
+    // ${roomname}
+    // ${text}
+    $message = $('<div class="message"></div>');
+    $messageBody = $(`<div>${text}</div>`);
+    $username.appendTo($message);
+    $messageBody.appendTo($message);
     $message.appendTo('#chats');
+  },
+
+  filterMessagesByRoom: function(filteredRoom) {
+    let selectedRoom = filteredRoom;
+    app.clearMessages();
+    var data = app.currentData.results;
+    if (selectedRoom === 'Default room') {
+      app.handleRefresh(app.currentData);
+    }
+    for (var i = 0; i < data.length; i++) {
+      if (selectedRoom === data[i].roomname) {
+        app.renderMessage(data[i]);
+      }
+    }
   },
 
   renderRoom: function (roomName) {
@@ -71,12 +127,42 @@ var app = {
     app.currentRoomName = roomName;
     if (roomName === 'Add a room!') {
       var newRoom = prompt('GIMME NAME!');
+      app.allRooms[newRoom] = newRoom;
       app.renderRoom(newRoom);
+    } else {
+      app.filterMessagesByRoom(roomName);
     }
   },
 
-  handleUsernameClick: function () {
-    console.log('click');
+  filterExistingRooms: function(data) {
+    for (let i = 0; i < data.results.length; i++) {
+      let newRoomName = data.results[i].roomname;
+      if (newRoomName && !app.allRooms[newRoomName]) {
+        //debugger;
+        for (let j = 0; j < newRoomName.length; j++) {
+          if (newRoomName[j] === '<' || newRoomName[j] === '>' || newRoomName[j] === '&' || newRoomName[j] === '"' || newRoomName[j] === '\'' || newRoomName[j] === '\\' || newRoomName[j] === '/') {
+            newRoomName = 'badRoomName';
+            break;
+          }
+        }
+        if (newRoomName !== 'badRoomName') {
+          app.allRooms[newRoomName] = newRoomName;
+          app.renderRoom(newRoomName);
+        } 
+      }
+    }
+  },
+
+  handleUsernameClick: function (username) {
+    // var username = $('.username').text();
+    if (!app.friends[username]) {
+      app.friends[username] = true;
+    } else {
+      app.friends[username] = !(app.friends[username]);
+    }
+    app.handleRefresh(app.currentData);
+    console.log(app.friends);
+    console.log(username);
   },
 
   handleSubmit: function (e) {
@@ -90,8 +176,18 @@ var app = {
     };
     app.send(newMessage);
     // submit shoudl call refresh method
-    console.log(newMessage);
+    
+    // app.handleRefresh(app.currentData);
   },
+
+  handleRefresh: function(newData) {
+    app.clearMessages();
+    var data = newData.results;
+    for (var i = 0; i < data.length; i++) {
+      app.renderMessage(data[i]);
+
+    }
+  }
 };
 
 $(app.init);
